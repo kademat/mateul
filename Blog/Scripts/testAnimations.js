496136c9e706130542df4077d93114bc5401d6c3
Mateusz Kamiński-Drąg <mateusz@trustbuddy.com>
2014-10-08 13:16:12 +0200

    Fixed hover on  navbar

--- Blog/Scripts/testAnimations.js
+++ Blog/Scripts/testAnimations.js
@@ -16,4 +16,9 @@
           $(".glyphicon").css('color', '');
       }
     );
+
+    $('.nav li a').on('click', function () {
+        $(this).parent().parent().find('.active').removeClass('active');
+        $(this).parent().addClass('active');
+    });
 });
