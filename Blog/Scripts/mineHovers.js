fc00f5347655d0eb611bb509507f399a486ed172
Mateusz <kademat@gmail.com>
2014-10-13 01:26:40 +0200

    commit with fixed table of contents - it's ready to add the content of tutorials, but need to be clear

--- Blog/Scripts/mineHovers.js
+++ Blog/Scripts/mineHovers.js
@@ -44,18 +44,6 @@
       }
     );
 
-    //Executes your code when the DOM is ready.  Acts the same as $(document).ready().
-    $(function () {
-        //Calls the tocify method on your HTML div.
-        $("#toc").tocify();
-    });
-
-    //for navigation
-    $('body').scrollspy({
-        target: '.bs-docs-sidebar',
-        offset: 40
-    });
-
     //wow
     new WOW().init();
 });
