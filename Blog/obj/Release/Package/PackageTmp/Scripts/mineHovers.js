5bb22509493f3086502d6299e6a80c94f6858940
Mateusz <kademat@gmail.com>
2015-04-25 23:33:32 +0200

    Remove running version, design changes

--- Blog/obj/Release/Package/PackageTmp/Scripts/mineHovers.js
+++ Blog/obj/Release/Package/PackageTmp/Scripts/mineHovers.js
@@ -1,32 +1,49 @@
 ﻿$(document).ready(function () {
 
-    //navigation glyphicon on the top
+    //change glyphicon color and animate it on hover
     $(".navigation").hover(
       function () {
           $(this).find('.glyphicon').css('color', 'darkgreen');
+          $(this).find('.glyphicon').addClass('animated rubberBand');
       },
       function () {
           $(".glyphicon").css('color', '');
+          $(this).find('.glyphicon').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', $(this).find('.glyphicon').removeClass('animated rubberBand'));
       }
     );
 
-    //navigation glyphicon on the nav
-    $("li").hover(
+    //links
+    $(".animate-img").hover(
       function () {
-          $(this).find('.glyphicon').css('color', 'darkgreen');
+          $(this).find('img').addClass('animated tada');
       },
       function () {
-          $(".glyphicon").css('color', '');
+          $(this).find('img').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', $(this).find('img').removeClass('animated tada'));
       }
     );
 
-    //for certification list in about page
+    //social
+    $(".animate-badge").hover(
+      function () {
+          $(this).addClass('animated tada');
+      },
+      function () {
+          $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', $(this).removeClass('animated tada'));
+      }
+    );
+
+    //for certification list in about page (small one)
     $(".certification").hover(
       function () {
           $(this).find('.glyphicon').css('color', 'darkblue');
+          $(this).find('.glyphicon').addClass('animated flipInX');
       },
       function () {
           $(".glyphicon").css('color', '');
+          $('.glyphicon').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', $('.glyphicon').removeClass('animated flipInX'));
       }
     );
+
+    //wow
+    new WOW().init();
 });
